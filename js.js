b5d6d75c24e2f8bac89d5c17cd2ec9fadb935db3
Daniil Dudariev <dudarev.daniel@gmail.com>
2019-10-13 20:18:51 +0300

    added loop and conditions.

--- js.js
+++ js.js
@@ -8,4 +8,26 @@ let guesses = 0;
 
 let isSunk = false;
 
+while (isSunk === false) {
+    guess = prompt('Ready, aim, Fire! (enter a number 0-6):');
+    if (guess < 0 || guess > 6) {
+        alert("Pleas enter a valid cell number!");
+    } else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert('Injured!');
+            hits = hits + 1;
+            if (hits === 3) {
+                isSunk = true;
+                alert('You sank my battleship!');
+            }
+        } else {
+            alert('You missed :(');
+        }
+    }
+}
+let stats = 'You took ' + guesses + ' guesses to sink the battleship, ' +
+    'which means your shooting accuracy was ' + (3 / guesses);
+alert(stats);
 
